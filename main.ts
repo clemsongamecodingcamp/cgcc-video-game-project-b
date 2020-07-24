0d8ee01e4449a376e9022400aa599a9cfa2df62f
ewokswagger <44028047+ewokswagger@users.noreply.github.com>
2020-07-24 15:43:15 -0400

    Added random duck spawning.

--- main.ts
+++ main.ts
@@ -1,14 +1,40 @@
 game.onGameUpdateWithHeading(function () {
+    if (Math.mod(game.runtime(), 2000) <= 50) {
+        SpawnDuck()
+    }
     controller.moveSprite(playerSprite)
 })
 function Start_Screen () {
     game.showLongText("This is my game. - By a Clemson First-Year Student", DialogLayout.Bottom)
 }
+function SpawnDuck () {
+    duckSprite = sprites.create(img`
+        . . . . . . . . . . b 5 b . . . 
+        . . . . . . . . . b 5 b . . . . 
+        . . . . . . b b b b b b . . . . 
+        . . . . . b b 5 5 5 5 5 b . . . 
+        . . . . b b 5 d 1 f 5 5 d f . . 
+        . . . . b 5 5 1 f f 5 d 4 c . . 
+        . . . . b 5 5 d f b d d 4 4 . . 
+        . b b b d 5 5 5 5 5 4 4 4 4 4 b 
+        b d d d b b d 5 5 4 4 4 4 4 b . 
+        b b d 5 5 5 b 5 5 5 5 5 5 b . . 
+        c d c 5 5 5 5 d 5 5 5 5 5 5 b . 
+        c b d c d 5 5 b 5 5 5 5 5 5 b . 
+        . c d d c c b d 5 5 5 5 5 d b . 
+        . . c b d d d d d 5 5 5 b b . . 
+        . . . c c c c c c c c b b . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Enemy)
+    tiles.placeOnTile(duckSprite, tiles.getTileLocation(randint(0, 9), 2))
+}
+let duckSprite: Sprite = null
 let playerSprite: Sprite = null
+let diff = 1
 scene.centerCameraAt(0, 84)
 info.setScore(0)
 info.setLife(3)
-info.startCountdown(10)
+info.startCountdown(9999999999)
 playerSprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . 6 6 6 6 . . . . . . 
@@ -27,24 +53,7 @@ playerSprite = sprites.create(img`
     . . . . . . 6 6 6 6 . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
-let duckSprite = sprites.create(img`
-    . . . . . . . . . . b 5 b . . . 
-    . . . . . . . . . b 5 b . . . . 
-    . . . . . . b b b b b b . . . . 
-    . . . . . b b 5 5 5 5 5 b . . . 
-    . . . . b b 5 d 1 f 5 5 d f . . 
-    . . . . b 5 5 1 f f 5 d 4 c . . 
-    . . . . b 5 5 d f b d d 4 4 . . 
-    . b b b d 5 5 5 5 5 4 4 4 4 4 b 
-    b d d d b b d 5 5 4 4 4 4 4 b . 
-    b b d 5 5 5 b 5 5 5 5 5 5 b . . 
-    c d c 5 5 5 5 d 5 5 5 5 5 5 b . 
-    c b d c d 5 5 b 5 5 5 5 5 5 b . 
-    . c d d c c b d 5 5 5 5 5 d b . 
-    . . c b d d d d d 5 5 5 b b . . 
-    . . . c c c c c c c c b b . . . 
-    . . . . . . . . . . . . . . . . 
-    `, SpriteKind.Enemy)
+playerSprite.setPosition(80, 110)
 tiles.setTilemap(tiles.createTilemap(hex`1000100003030303030303030303030303030303050505050505050505050503030303030204020402020402040205030303030307070707070707070707050303030303010601060101060106010503030303030101010101010101010105030303030301010101010101010101050303030303010101010101010101010503030303030101010101010101010105030303030305050505050505050505050303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303`, img`
     . . . . . . . . . . . . . . . . 
     2 2 2 2 2 2 2 2 2 2 2 . . . . . 
@@ -63,4 +72,4 @@ tiles.setTilemap(tiles.createTilemap(hex`100010000303030303030303030303030303030
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, [myTiles.transparency16,myTiles.tile1,myTiles.tile2,myTiles.tile4,myTiles.tile3,myTiles.tile5,myTiles.tile7,myTiles.tile8], TileScale.Sixteen))
-tiles.placeOnTile(duckSprite, tiles.getTileLocation(randint(0, 9), 2))
+SpawnDuck()
