a63c47f1d607c8edee4449410124bfd26b64f50f
ewokswagger <44028047+ewokswagger@users.noreply.github.com>
2020-07-27 14:25:12 -0400

    Added animations

--- main.ts
+++ main.ts
@@ -1,3 +1,140 @@
+enum ActionKind {
+    Walking,
+    Idle,
+    Jumping
+}
+function initAnimatations () {
+    catWalkLeft = animation.createAnimation(ActionKind.Walking, 1000)
+    catWalkLeft.addAnimationFrame(img`
+        . . . . . . . . . . . . . . 
+        e e e . . . . e e e . . . . 
+        c d d c . . c d d c . . . . 
+        c b d d f f d d b c . . . . 
+        c 3 b d d b d b 3 c . . . . 
+        f b 3 d d d d 3 b f . . . . 
+        e d d d d d d d d e . . . . 
+        e d f d d d d f d e b f b . 
+        f d d f d d f d d f f d f . 
+        f b d d b b d d 2 b f d f . 
+        . f 2 2 2 2 2 2 d b d b f . 
+        . f d d d d d d d f f f . . 
+        . f d b d f f f d f . . . . 
+        . . f f f f . . f f . . . . 
+        `)
+    catWalkLeft.addAnimationFrame(img`
+        . . . . . . . . . . . . . . 
+        e e e . . . . e e e . . . . 
+        c d d c . . c d d c . . . . 
+        c b d d f f d d b c . . . . 
+        c 3 b d d b d b 3 c . . . . 
+        f b 3 d d d d 3 b f . . . . 
+        e d d d d d d d d e . . . . 
+        e d f d d d d f d e . b f b 
+        f d d f d d f d d f . f d f 
+        f b d d b b d d 2 b f f d f 
+        . f 2 2 2 2 2 2 d b b d b f 
+        . f d d d d d d d f f f f . 
+        . . f d b d f d f . . . . . 
+        . . . f f f f f f . . . . . 
+        `)
+    catWalkLeft.addAnimationFrame(img`
+        e e e . . . . e e e . . . . 
+        c d d c . . c d d c . . . . 
+        c b d d f f d d b c . . . . 
+        c 3 b d d b d b 3 c . . . . 
+        f b 3 d d d d 3 b f . . . . 
+        e d d d d d d d d e . . . . 
+        e d f d d d d f d e . b f b 
+        f d d f d d f d d f . f d f 
+        f b d d b b d d 2 f . f d f 
+        . f 2 2 2 2 2 2 b b f f d f 
+        . f b d d d d d d b b d b f 
+        . f d d d d d b d d f f f . 
+        . f d f f f d f f d f . . . 
+        . f f . . f f . . f f . . . 
+        `)
+    catWalkRight = animation.createAnimation(ActionKind.Walking, 1000)
+    catWalkRight.addAnimationFrame(img`
+        . . . . . . . . . . . . . . 
+        e e e . . . . e e e . . . . 
+        c d d c . . c d d c . . . . 
+        c b d d f f d d b c . . . . 
+        c 3 b d d b d b 3 c . . . . 
+        f b 3 d d d d 3 b f . . . . 
+        e d d d d d d d d e . . . . 
+        e d f d d d d f d e b f b . 
+        f d d f d d f d d f f d f . 
+        f b d d b b d d 2 b f d f . 
+        . f 2 2 2 2 2 2 d b d b f . 
+        . f d d d d d d d f f f . . 
+        . f d b d f f f d f . . . . 
+        . . f f f f . . f f . . . . 
+        `)
+    catWalkRight.addAnimationFrame(img`
+        . . . . . . . . . . . . . . 
+        e e e . . . . e e e . . . . 
+        c d d c . . c d d c . . . . 
+        c b d d f f d d b c . . . . 
+        c 3 b d d b d b 3 c . . . . 
+        f b 3 d d d d 3 b f . . . . 
+        e d d d d d d d d e . . . . 
+        e d f d d d d f d e . b f b 
+        f d d f d d f d d f . f d f 
+        f b d d b b d d 2 b f f d f 
+        . f 2 2 2 2 2 2 d b b d b f 
+        . f d d d d d d d f f f f . 
+        . . f d b d f d f . . . . . 
+        . . . f f f f f f . . . . . 
+        `)
+    catWalkRight.addAnimationFrame(img`
+        e e e . . . . e e e . . . . 
+        c d d c . . c d d c . . . . 
+        c b d d f f d d b c . . . . 
+        c 3 b d d b d b 3 c . . . . 
+        f b 3 d d d d 3 b f . . . . 
+        e d d d d d d d d e . . . . 
+        e d f d d d d f d e . b f b 
+        f d d f d d f d d f . f d f 
+        f b d d b b d d 2 f . f d f 
+        . f 2 2 2 2 2 2 b b f f d f 
+        . f b d d d d d d b b d b f 
+        . f d d d d d b d d f f f . 
+        . f d f f f d f f d f . . . 
+        . f f . . f f . . f f . . . 
+        `)
+    catIdleLeft = img`
+        e e e . . . . e e e . . . . 
+        c d d c . . c d d c . . . . 
+        c b d d f f d d b c . . . . 
+        c 3 b d d b d b 3 c . . . . 
+        f b 3 d d d d 3 b f . . . . 
+        e d d d d d d d d e . . . . 
+        e d f d d d d f d e . b f b 
+        f d d f d d f d d f . f d f 
+        f b d d b b d d 2 f . f d f 
+        . f 2 2 2 2 2 2 b b f f d f 
+        . f b d d d d d d b b d b f 
+        . f d d d d d b d d f f f . 
+        . f d f f f d f f d f . . . 
+        . f f . . f f . . f f . . . 
+        `
+    catIdleRight = img`
+        e e e . . . . e e e . . . . 
+        c d d c . . c d d c . . . . 
+        c b d d f f d d b c . . . . 
+        c 3 b d d b d b 3 c . . . . 
+        f b 3 d d d d 3 b f . . . . 
+        e d d d d d d d d e . . . . 
+        e d f d d d d f d e . b f b 
+        f d d f d d f d d f . f d f 
+        f b d d b b d d 2 f . f d f 
+        . f 2 2 2 2 2 2 b b f f d f 
+        . f b d d d d d d b b d b f 
+        . f d d d d d b d d f f f . 
+        . f d f f f d f f d f . . . 
+        . f f . . f f . . f f . . . 
+        `
+}
 game.onGameUpdateWithHeading(function () {
     if (Math.mod(game.runtime(), 2000) <= 40) {
         if (spawnRate > randint(0, 999)) {
@@ -5,6 +142,13 @@ game.onGameUpdateWithHeading(function () {
         }
     }
     controller.moveSprite(playerSprite)
+    info.setScore(headingRight)
+    if (sprites.heading(playerSprite) > 15 && sprites.heading(playerSprite) < 165) {
+        headingRight = 1
+    }
+    if (sprites.heading(playerSprite) > 195 && sprites.heading(playerSprite) < 345) {
+        headingRight = 0
+    }
 })
 function Start_Screen () {
     game.showLongText("This is my game. - By a Clemson First-Year Student", DialogLayout.Bottom)
@@ -34,7 +178,12 @@ function NormalDiff () {
     spawnRate = 500
 }
 let duckSprite: Sprite = null
+let headingRight = 0
 let spawnRate = 0
+let catIdleRight: Image = null
+let catIdleLeft: Image = null
+let catWalkRight: animation.Animation = null
+let catWalkLeft: animation.Animation = null
 let playerSprite: Sprite = null
 let diff = 1
 scene.centerCameraAt(0, 84)
@@ -43,25 +192,23 @@ info.setScore(0)
 info.setLife(3)
 info.startCountdown(6000)
 playerSprite = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . 6 6 6 6 . . . . . . 
-    . . . . 6 6 6 5 5 6 6 6 . . . . 
-    . . . 7 7 7 7 6 6 6 6 6 6 . . . 
-    . . 6 7 7 7 7 8 8 8 1 1 6 6 . . 
-    . . 7 7 7 7 7 8 8 8 1 1 5 6 . . 
-    . 6 7 7 7 7 8 8 8 8 8 5 5 6 6 . 
-    . 6 7 7 7 8 8 8 6 6 6 6 5 6 6 . 
-    . 6 6 7 7 8 8 6 6 6 6 6 6 6 6 . 
-    . 6 8 7 7 8 8 6 6 6 6 6 6 6 6 . 
-    . . 6 8 7 7 8 6 6 6 6 6 8 6 . . 
-    . . 6 8 8 7 8 8 6 6 6 8 6 6 . . 
-    . . . 6 8 8 8 8 8 8 8 8 6 . . . 
-    . . . . 6 6 8 8 8 8 6 6 . . . . 
-    . . . . . . 6 6 6 6 . . . . . . 
-    . . . . . . . . . . . . . . . . 
+    e e e . . . . e e e . . . . 
+    c d d c . . c d d c . . . . 
+    c b d d f f d d b c . . . . 
+    c 3 b d d b d b 3 c . . . . 
+    f b 3 d d d d 3 b f . . . . 
+    e d d d d d d d d e . . . . 
+    e d f d d d d f d e . b f b 
+    f d d f d d f d d f . f d f 
+    f b d d b b d d 2 f . f d f 
+    . f 2 2 2 2 2 2 b b f f d f 
+    . f b d d d d d d b b d b f 
+    . f d d d d d b d d f f f . 
+    . f d f f f d f f d f . . . 
+    . f f . . f f . . f f . . . 
     `, SpriteKind.Player)
 playerSprite.setPosition(80, 110)
-tiles.setTilemap(tiles.createTilemap(hex`1000100003030303030303030303030303030303050505050505050505050503030303030204020402020402040205030303030307070707070707070707050303030303010601060101060106010503030303030101010101010101010105030303030301010101010101010101050303030303010101010101010101010503030303030101010101010101010105030303030305050505050505050505050303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303`, img`
+tiles.setTilemap(tiles.createTilemap(hex`1000100003030303030303030303030303030303050505050505050505050503030303030204020402020402040205030303030306060606060606060606050303030303010701070101070107010503030303030101010101010101010105030303030301010101010101010101050303030303010101010101010101010503030303030101010101010101010105030303030305050505050505050505050303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303`, img`
     . . . . . . . . . . . . . . . . 
     2 2 2 2 2 2 2 2 2 2 2 . . . . . 
     . . . . . . . . . . 2 . . . . . 
@@ -78,5 +225,5 @@ tiles.setTilemap(tiles.createTilemap(hex`100010000303030303030303030303030303030
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
-    `, [myTiles.transparency16,myTiles.tile1,myTiles.tile2,myTiles.tile4,myTiles.tile3,myTiles.tile5,myTiles.tile7,myTiles.tile8], TileScale.Sixteen))
+    `, [myTiles.transparency16,myTiles.tile1,myTiles.tile2,myTiles.tile4,myTiles.tile3,myTiles.tile5,myTiles.tile8,sprites.dungeon.buttonOrange], TileScale.Sixteen))
 SpawnDuck()
