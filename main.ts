0a4a1d79a46bde6568e86779d1c16ebef2003fa4
ewokswagger <44028047+ewokswagger@users.noreply.github.com>
2020-07-31 13:40:28 -0400

    final present

--- main.ts
+++ main.ts
@@ -3,6 +3,10 @@ enum ActionKind {
     Idle,
     Jumping
 }
+scene.onOverlapTile(SpriteKind.Enemy, sprites.dungeon.hazardHole, function (sprite, location) {
+    sprite.destroy()
+    info.changeScoreBy(100)
+})
 function animate () {
     catWalkLeft = animation.createAnimation(ActionKind.Walking, 1000)
     catWalkLeft.addAnimationFrame(img`
@@ -121,6 +125,7 @@ game.onGameUpdateWithHeading(function () {
                 . . . . . . . . . . . . . . . . 
                 . . . . . . . . . . . . . . . . 
                 `, thisDuck, 0, laserSpeed)
+            music.playTone(220, music.beat(BeatFraction.Sixteenth))
             laser.setFlag(SpriteFlag.Ghost, true)
             lasers.push(laser)
         }
@@ -144,6 +149,14 @@ game.onGameUpdateWithHeading(function () {
             hatchTime.removeAt(index2)
         }
     }
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.buttonOrange, function (sprite, location) {
+    buttonX.push(scene.getTileColCoordinate(location))
+    buttonTime.push(game.runtime() + 5000)
+    hatchX.push(scene.getTileColCoordinate(location))
+    hatchTime.push(game.runtime() + 500)
+    tiles.setTileAt(location, sprites.dungeon.buttonTealDepressed)
+    tiles.setTileAt(tiles.getTileLocation(scene.getTileColCoordinate(location), 2), sprites.dungeon.hazardHole)
 })
 function EasyDiff () {
     spawnRate = 250
@@ -183,21 +196,9 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, ot
     info.changeLifeBy(-1)
     otherSprite.destroy()
 })
-scene.onOverlapTile(SpriteKind.Enemy, sprites.dungeon.hazardHole, function (sprite, location) {
-    sprite.destroy()
-    info.changeScoreBy(100)
-})
 function Start_Screen () {
     game.showLongText("This is my game. - By a Clemson First-Year Student", DialogLayout.Bottom)
 }
-scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.buttonOrange, function (sprite, location) {
-    buttonX.push(scene.getTileColCoordinate(location))
-    buttonTime.push(game.runtime() + 5000)
-    hatchX.push(scene.getTileColCoordinate(location))
-    hatchTime.push(game.runtime() + 500)
-    tiles.setTileAt(location, sprites.dungeon.buttonTealDepressed)
-    tiles.setTileAt(tiles.getTileLocation(scene.getTileColCoordinate(location), 2), sprites.dungeon.hazardHole)
-})
 function NormalDiff () {
     spawnRate = 500
     laserSpeed = 10
@@ -240,7 +241,7 @@ if (diff == 0) {
 animate()
 info.setScore(0)
 info.setLife(3)
-info.startCountdown(6000)
+info.startCountdown(300)
 playerSprite = sprites.create(img`
     e e e . . . . e e e . . . . 
     c d d c . . c d d c . . . . 
